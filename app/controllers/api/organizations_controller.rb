6f98c602b9696d5536de38e220942c7da0bb4e86
vanillafrosty <dwu.inq@gmail.com>
2018-07-07 21:15:41 -0400

    add more api calls to grab contributor count

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -6,16 +6,18 @@ class Api::OrganizationsController < ApplicationController
     #filter the data
     out = []
     repos.each do |repo|
+      contributors = JSON.parse(RestClient.get("https://api.github.com/repos/appacademy/#{repo["name"]}/contributors"))
       filteredRepo = {
         name: repo["name"],
         full_name: repo["full_name"],
         forks_count: repo["forks_count"],
-        stargazers_count: repo["stargazers_count"]
+        stargazers_count: repo["stargazers_count"],
+        contributors_count: contributors.length
       }
       out.push(filteredRepo)
+
     end
     render json: out
-
   end
 
 
